1256ce85851b5471362182acaff4b0c240a89f10
shivalisinghai <shivalis.singhai@gmail.com>
2023-07-12 17:11:58 +0530

    formatting

--- js/fetch.js
+++ js/fetch.js
@@ -1,19 +1,18 @@
 async function fetchDataJSON() {
-
-const response = await fetch('/js/code-test.json');
-
-const data = await response.json();
-return data;
+    const response = await fetch('/js/code-test.json');
+    const data = await response.json();
+    return data;
 }
+
 fetchDataJSON().then(data => {
-const articleContainer = document.getElementById ('article-container');
-data.articles.forEach(article => {
-articleContainer.innerHTML += renderArticle(article) ;
-})
+    const articleContainer = document.getElementById('article-container');
+    data.articles.forEach(article => {
+        articleContainer.innerHTML += renderArticle(article);
+    })
 })
 
 function renderArticle(article) {
-return`
+    return `
 <div class="article-card">
 <div class="inner-article">
 <p class="article">${article.primarySectionRouteName}</p>
